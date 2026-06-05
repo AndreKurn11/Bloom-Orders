4f90899a8259b22f9cf6aed4842c3f1d9028db61
AndreKurn11 <muhammadandre110311@gmail.com>
2026-06-05 15:41:00 +0700

    Integrasi Template UI

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('customers.menu');
 });
+
+Route::get('/cart', function () {
+    return view('customers.cart');
+})->name('cart');
+
+Route::get('/checkout', function () {
+    return view('customers.checkout');
+})->name('checkout');
