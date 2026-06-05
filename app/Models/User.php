669109904a4fe4e1a1b28074f4fed0e9e6829edf
AndreKurn11 <muhammadandre110311@gmail.com>
2026-06-05 15:01:41 +0700

    Pembuatan Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,24 +5,33 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
+        'fullname',
+        'email',
+        'phone',
+        'role_id',
+        'created_at',
+        'updated_at'
     ];
+    protected $dates = ['deleted_at'];
+
+    public function role() {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
