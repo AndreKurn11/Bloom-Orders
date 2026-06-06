50db930f93c2deaa592f6ae69219a07904a2db1d
AndreKurn11 <muhammadandre110311@gmail.com>
2026-06-06 10:05:19 +0700

    integrasi template dan pembuatan menu dashboard

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $orders = Order::all()->sortByDesc('created_at');
+        return view('admin.order.index', compact('orders'));
+    }
+}
